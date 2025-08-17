34c7d5f2c75221cb6ee33ab4d164c97a05d5df0d
nalinduash <nalindugra@gmail.com>
2025-08-17 10:57:32 +0530

    Finished Fonts

--- Scripts/fonts.sh
+++ Scripts/fonts.sh
@@ -3,4 +3,11 @@
 # Importing SH files
 source ./Scripts/common.sh
 
-oh-my-posh font install DejaVuSansMono
+# Downloading fonts
+oh-my-posh font install JetBrainsMono
+
+# Setting fonts in gnome
+gsettings set org.gnome.desktop.interface font-name "Adwaita Sans Regular 11"
+gsettings set org.gnome.desktop.interface document-font-name "Adwaita Sans Regular 11"
+gsettings set org.gnome.desktop.interface monospace-font-name "JetBrainsMono Nerd Font Regular 12"
+gsettings set org.gnome.desktop.wm.preferences titlebar-font "Adwaita Sans SemiBold 11"
