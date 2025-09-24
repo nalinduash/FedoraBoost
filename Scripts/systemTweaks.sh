b514624d6326766c409bfa2342074424f81a41db
nalinduash <nalindugra@gmail.com>
2025-09-24 12:40:39 +0530

    Fix some dependencies

--- Scripts/systemTweaks.sh
+++ Scripts/systemTweaks.sh
@@ -32,28 +32,28 @@ if echo "$GPU_INFO" | grep -qi 'Intel'; then HAS_INTEL=1; fi
 if echo "$GPU_INFO" | grep -qi 'AMD'; then HAS_AMD=1; fi
 
 # Intel setup
-logScriptSubHead "Installing drivers for Intel"
-if (( HAS_INTEL )); then
-  logInfo "Found an Intel GPU"
-  runCmd "sudo dnf swap -y libva-intel-media-driver intel-media-driver --allowerasing" "Installing Intel drivers..."
-  runCmd "sudo dnf install -y libva-intel-driver" "Installing Intel legacy drivers..."
-else
-  logPass "No Intel GPU detected; skipping Intel-specific setup."
-fi
+# logScriptSubHead "Installing drivers for Intel"
+# if (( HAS_INTEL )); then
+#  logInfo "Found an Intel GPU"
+#  runCmd "sudo dnf swap -y libva-intel-media-driver intel-media-driver --allowerasing" "Installing Intel drivers..."
+#  runCmd "sudo dnf install -y libva-intel-driver" "Installing Intel legacy drivers..."
+#else
+#  logPass "No Intel GPU detected; skipping Intel-specific setup."
+#fi
 
 # AMD setup
-logScriptSubHead "Installing drivers for AMD"
-if (( HAS_AMD )); then
-  logInfo "Found an AMD GPU"
+#logScriptSubHead "Installing drivers for AMD"
+#if (( HAS_AMD )); then
+#  logInfo "Found an AMD GPU"
   # 64-bit swaps
-  swapPackages "mesa-va-drivers" "mesa-va-drivers-freeworld"
-  swapPackages "mesa-vdpau-drivers" "mesa-vdpau-drivers-freeworld"
+#  swapPackages "mesa-va-drivers" "mesa-va-drivers-freeworld"
+#  swapPackages "mesa-vdpau-drivers" "mesa-vdpau-drivers-freeworld"
   # 32-bit swaps
-  swapPackages "mesa-va-drivers.i686" "mesa-va-drivers-freeworld.i686"
-  swapPackages "mesa-vdpau-drivers.i686" "mesa-vdpau-drivers-freeworld.i686"
-else
-  logPass "No AMD GPU detected; skipping AMD-specific swaps."
-fi
+#  swapPackages "mesa-va-drivers.i686" "mesa-va-drivers-freeworld.i686"
+#  swapPackages "mesa-vdpau-drivers.i686" "mesa-vdpau-drivers-freeworld.i686"
+#else
+#  logPass "No AMD GPU detected; skipping AMD-specific swaps."
+#fi
 
 
 # ======> Set UTC Time for dual boots
